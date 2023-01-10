520594b212aa9b7730c0c5e33cfd605bdc6738b7
Meli357 <wyck_f@hotmail.com>
2023-01-10 09:26:14 +0100

    add modules mjs

---
+++ output.mjs
@@ -0,0 +1,3 @@
+import Greeting from "./script.mjs";
+
+Greeting("Melissa");
---
+++ script.mjs
@@ -0,0 +1,8 @@
+// Create a script that uses export default to export a function.
+// Create another script that uses import to import the function and then calls it.
+
+function Greeting(nome){
+    console.log(`Have a good day ${nome}`)
+}
+
+export default Greeting
