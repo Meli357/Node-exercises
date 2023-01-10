bc30667cb2bdab19817c4824f62bca206981742a
Meli357 <wyck_f@hotmail.com>
2023-01-10 09:18:30 +0100

    code to generate randomID

---
+++ random.js
@@ -0,0 +1,8 @@
+// Use the Node.js REPL to list the methods provided by the Node.js core crypto module. Use one of these methods to generate a random ID.
+
+const crypto = require("crypto");
+
+console.log(crypto.randomUUID());
+
+// output:
+// b250f130-dbf9-49a2-a666-27185412f378
