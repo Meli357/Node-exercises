79cc04df5ac3509e62425adabe840ceb16f6036a
Meli357 <106169198+Meli357@users.noreply.github.com>
2023-01-11 22:45:32 +0100

    http request with curl

---
+++ output.js
@@ -0,0 +1,9 @@
+// Make an HTTP request with curl that shows the response headers for this URL: https://jsonplaceholder.typicode.com/posts/1/comments
+
+// What is the value of the content-type response header?
+
+/* curl -v https://jsonplaceholder.typicode.com/posts/1/comments */
+
+// OUTPUT:
+// DETTAGLIATO: GET with 0-byte payload
+// DETTAGLIATO: received -1-byte response of content type       application/json; charset=utf-8
